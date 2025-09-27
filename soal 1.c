53eb97223c7e9ceca0b62fca5950866679672e08
Arta Wijaya <artawijaya2004@gmail.com>
2025-09-27 11:21:22 +0800

    oke

---
+++ soal 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int J;
+    scanf("%d", &J);
+    float T;
+    scanf("%f", &T);
+    float kecepatan;
+    kecepatan = J/T;
+    printf("%.3f", kecepatan);
+   
+}
---
+++ soal 3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int awal;
+    int akhir;
+    int total_semua;
+    
+    scanf ("%d", &awal);
+    
+    scanf ("%d", &akhir);
+    total_semua=awal;
+     while(awal < akhir){
+        awal++;
+        total_semua += awal;
+    } 
+    printf ("%d", total_semua);
+   return 0;
+}
